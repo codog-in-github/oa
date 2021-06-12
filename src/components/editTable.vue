9954c2a65a9d72bd2412338daa64624ac928aec7
zhaojy <42832331+spSAKURA@users.noreply.github.com>
2021-06-12 19:59:22 +0800

    get table config data complete

---
+++ src/components/editTable.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <el-table :data='data'> 
+            <el-table-column v-for="{id, col, name} in col" :key="id"
+                :prop="col"
+                :label="name">
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+<script>
+export default {
+    props:['col', 'data'],
+}
+</script>
