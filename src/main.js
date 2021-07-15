c235267a8fa640127642ddeae2219cb8c27781df
zjy <zhaojangyu@outlook.com>
2021-07-15 12:58:53 +0800

    删除loading

--- src/main.js
+++ src/main.js
@@ -6,35 +6,12 @@ import 'element-ui/lib/theme-chalk/index.css'
 import ElementUI from 'element-ui'
 import {Api} from './api/main'
 import './assets/style/public.css'
-import Loading from './components/Loading.vue'
+import locale from 'element-ui/lib/locale/lang/en'
 
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
+Vue.use(ElementUI,{locale});
 Vue.use(Api);
 
-Vue.directive('loading',{
-  bind(el){
-    const LoadControl = Vue.extend(Loading);
-    const LoadngComp = new LoadControl().$mount();
-    // console.log(el);
-    el._loadingDom = LoadngComp;
-    el.style.position = el.style.position || 'relative';
-  },
-  inserted(el,binding){
-    if(binding.value !== binding.oldValue){
-      if(binding.value){
-        el.appendChild(el._loadingDom.$el);
-      }
-    }
-  },
-  update(el, binding){
-    if(binding.value !== binding.oldValue){
-      binding.value? 
-        el.appendChild(el._loadingDom.$el) :
-        el.removeChild(el._loadingDom.$el);
-    }
-  },
-});
 
 export default new Vue({
   el:'#app',
