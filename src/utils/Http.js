d30c629de97c743fcb8a985757351e2d8569f1b4
zjy <zhaojangyu@gmail.com>
2022-01-13 16:22:24 +0800

    请求类

--- src/utils/Http.js
+++ src/utils/Http.js
@@ -1,9 +1,65 @@
 import axios from 'axios'
+import { stringify } from 'qs'
+import { STATE_CODE_NO_LOGIN, STATE_CODE_SUCCESS } from '@/constant/API'
+import { urlParamsStringify } from '.'
+import { Message } from 'element-ui'
+import { router } from '@/router/main'
+
+const REQUEST_DEFALUT_CONFIG = {
+    withCredentials: true
+}
+
 /**
  * @description http 请求类
  */
 export class Http {
-    static get (params) {
+    static interceptor = {
+        request () {},
+        response (res) {
+            const { data, status } = res
+            if (status !== 200) {
+                Message.error('ERROR')
+                return Promise.reject('ERROR')
+            }
 
+            if (data.error === STATE_CODE_NO_LOGIN) {
+                Message.error('please login')
+                router.push('/login')
+                return Promise.reject(data.message)
+            }
+
+            if (data.error === STATE_CODE_SUCCESS) {
+                return Promise.resolve(data.data)
+            } else {
+                Message.error(data.message)
+                return Promise.reject(data.message)
+            }
+        }
+    }
+
+    static get (config = {}) {
+        const {
+            url = '',
+            params = {}
+        } = config
+
+        return axios.get(
+            url + urlParamsStringify(params),
+            { ...REQUEST_DEFALUT_CONFIG }
+        )
+    }
+
+    static post (config = {}) {
+        const {
+            url = '',
+            params = {}
+        } = config
+
+        return axios.post(
+            url, stringify(params),
+            { ...REQUEST_DEFALUT_CONFIG }
+        )
     }
 }
+
+axios.interceptors.response.use(Http.interceptor.response)
